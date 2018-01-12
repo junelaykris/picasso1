a906e21bc9e5c4d9b216eda1fbd3e6c3b4f9454e
junelaykris <junelay6695@gmail.com>
2018-01-12 10:30:12 +0630

    first commit

---
+++ app/src/main/java/com/example/mit_372/picasso1/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.mit_372.picasso1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        Picasso.with(this).load("https://static.tumblr.com/2d06876adc91e4c88344aa9924b28a73/n93hc6n/u07n86n7c/tumblr_static_filename_640_v2.jpg")
+                .placeholder(R.drawable.placeholder)
+                .error(R.drawable.error)
+                .resize(600,600)
+                .into(imageView);
+
+
+    }
+}
